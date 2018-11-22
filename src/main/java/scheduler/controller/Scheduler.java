ae5e0a9bd98c6287e7ff1894032aeb99ae0f5137
mikhael-artur <mikhael.artur.d@gmail.com>
2018-11-22 23:22:13 +0700

    Completed SchedulerActionListener Class

--- src/main/java/scheduler/controller/Scheduler.java
+++ src/main/java/scheduler/controller/Scheduler.java
@@ -4,15 +4,8 @@ import java.util.List;
 import scheduler.model.Classroom;
 import scheduler.model.Constraint;
 import scheduler.model.Lecture;
-<<<<<<< HEAD
-import scheduler.model.type.Time;
-
-import java.time.DayOfWeek;
-import java.util.Arrays;
-=======
 import scheduler.model.Lecturer;
 import scheduler.model.Schedule;
->>>>>>> 86dcbcd18ec0e9fd082f4670bdcda74a151c9056
 
 public class Scheduler {
 
@@ -23,16 +16,6 @@ public class Scheduler {
     private Schedule[] schedules;
 
     public Scheduler() {}
-<<<<<<< HEAD
-    public void setLectures(Lecture... lectures) {
-        this.lectures = lectures;
-    }
-    public void setClassrooms(Classroom... classrooms) {
-        this.classrooms = classrooms;
-    }
-    public void setConstraints(Constraint... constraints) {
-        this.constraints = constraints;
-=======
 
     public void addLecture(Lecture lecture) {
         this.lectures.add(lecture);
@@ -48,83 +31,9 @@ public class Scheduler {
 
     public void addLecturer(Lecturer lecturer){
         this.lecturers.add(lecturer);
->>>>>>> 86dcbcd18ec0e9fd082f4670bdcda74a151c9056
     }
-
-    /* Scheduling */
 
     public Schedule[] schedule() {
-        this.schedules = new Schedule[this.lectures.length];
-        Arrays.fill(schedules, null);
-
-        assignSchedule(0);
-        return this.schedules;
-    }
-
-    private boolean assignSchedule(int lectureIdx) {
-        // Assignment done
-        if(lectureIdx == this.lectures.length) return true;
-
-        // Assign with all possible classrooms and available time
-        boolean assigned;
-
-        Lecture lecture = this.lectures[lectureIdx];
-        Time[] availability = lecture.lecturer.availability;
-
-        for(Classroom classroom : this.classrooms) {
-            if(!classroom.canAccommodate(lecture.students, lecture.requirements)) continue;
-
-            for(Time time : availability) {
-                for(int start = time.start; start < time.end; start++) {
-                    if(!(isClassroomAvailableWithinSchedule(lectureIdx, classroom, time.day, start) && withinConstraints(lecture, time.day, start))) continue;
-
-                    this.schedules[lectureIdx] = new Schedule(lecture, classroom, new Time(time.day, start, start + 1));
-                    assigned = assignSchedule(lectureIdx + 1);
-
-                    if(assigned) return true;
-                    else this.schedules[lectureIdx] = null;
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private boolean isClassroomAvailableWithinSchedule(int lectureIdx, Classroom classroom, DayOfWeek day, int start) {
-        for(int idx = 0; idx < this.schedules.length; idx++) {
-            if(idx == lectureIdx) continue;
-
-            Schedule schedule = this.schedules[idx];
-            if(schedule == null) continue;
-
-            if(classroom == schedule.classroom && day == schedule.time.day && start == schedule.time.start) return false;
-        }
-
-        return true;
-    }
-
-    private boolean withinConstraints(Lecture lecture, DayOfWeek day, int start) {
-        for(Constraint constraint : this.constraints) {
-            if(!constraint.has(lecture)) continue;
-
-            for(Lecture otherLecture : constraint.lectures) {
-                if(otherLecture == lecture) continue;
-
-                int otherLectureIdx = findLectureIdx(otherLecture);
-                if(this.schedules[otherLectureIdx] == null) continue;
-
-                if(day == this.schedules[otherLectureIdx].time.day && start == this.schedules[otherLectureIdx].time.start) return false;
-            }
-        }
-
-        return true;
-    }
-
-    private int findLectureIdx(Lecture lecture) {
-        for(int idx = 0; idx < this.lectures.length; idx++) {
-            if(this.lectures[idx] == lecture) return idx;
-        }
-
-        return -1;
+        return null;
     }
 }
--- src/main/java/scheduler/controller/SchedulerActionListener.java
+++ src/main/java/scheduler/controller/SchedulerActionListener.java
@@ -1,18 +1,22 @@
 package scheduler.controller;
 
+import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.List;
 import scheduler.model.Classroom;
+import scheduler.model.Constraint;
 import scheduler.model.Lecture;
 import scheduler.model.Lecturer;
 import scheduler.model.enums.Facility;
 import scheduler.parser.SchedulerBaseListener;
+import scheduler.parser.SchedulerParser;
 import scheduler.parser.SchedulerParser.Create_classroomContext;
 import scheduler.parser.SchedulerParser.Create_lectureContext;
 import scheduler.parser.SchedulerParser.Create_lecturerContext;
 import scheduler.parser.SchedulerParser.Create_scheduleContext;
 import scheduler.parser.SchedulerParser.Facility_nameContext;
 import scheduler.parser.SchedulerParser.Make_constrainContext;
+import scheduler.model.type.Time;
 
 public class SchedulerActionListener extends SchedulerBaseListener {
   private Scheduler scheduler;
@@ -38,7 +42,7 @@ public class SchedulerActionListener extends SchedulerBaseListener {
     Classroom classroom = new Classroom(name, capacity, facilities_arr);
     scheduler.addClassroom(classroom);
 
-    System.out.printf("ADDED CLASSROOM %s WITH CAPACITY %d and FACILITIES %s\n", name, capacity, facilities);
+    System.out.printf("ADDED CLASSROOM %s WITH CAPACITY %d and FACILITIES %s.\n", name, capacity, facilities);
   }
 
   @Override
@@ -53,8 +57,19 @@ public class SchedulerActionListener extends SchedulerBaseListener {
     }
     Facility[] facilities_arr = facilities.toArray((new Facility[facilities.size()]));
 
-    //TODO Check if lecturer exist and get lecturer
     Lecturer lecturer = null;
+    int i = 0;
+    boolean found = false;
+    while (i <= this.scheduler.lecturers.size() && !found){
+      if (lecturer_name.equalsIgnoreCase(this.scheduler.lecturers.get(i).name)){
+        found = true;
+        lecturer = this.scheduler.lecturers.get(i);
+      }
+      i++;
+    }
+    if (!found){
+      throw new IllegalArgumentException("LECTURER " + lecturer_name + " DOES NOT EXIST! CREATE ONE.\n");
+    }
     Lecture lecture = new Lecture(name, lecturer, capacity, facilities_arr);
     scheduler.addLecture(lecture);
 
@@ -63,16 +78,48 @@ public class SchedulerActionListener extends SchedulerBaseListener {
 
   @Override
   public void exitCreate_lecturer(Create_lecturerContext ctx) {
+    List<Time> availabilities = new ArrayList<>();
+    for (SchedulerParser.Time_specificationContext t : ctx.time_specification()){
+      availabilities.add(new Time(
+        DayOfWeek.valueOf(t.K_DAYS().getText()),
+        Integer.parseInt(t.NUM(0).getText()),
+        Integer.parseInt(t.NUM(1).getText()))
+      );
+    }
+    Time[] availability_arr = availabilities.toArray((new Time[availabilities.size()]));
+    scheduler.addLecturer(new Lecturer(ctx.lecturer_name().getText(), availability_arr));
     super.exitCreate_lecturer(ctx);
+    System.out.printf("LECTURER %s ADDED.\n", ctx.lecturer_name().getText());
   }
 
   @Override
   public void exitCreate_schedule(Create_scheduleContext ctx) {
+    scheduler.schedule();
     super.exitCreate_schedule(ctx);
+    System.out.printf("SCHEDULE CREATED.\n");
   }
 
   @Override
   public void exitMake_constrain(Make_constrainContext ctx) {
+    List<Lecture> lectures = new ArrayList<>();
+    for (SchedulerParser.Class_nameContext l : ctx.class_name()){
+      int i = 0;
+      boolean found = false;
+      while (i <= this.scheduler.lectures.size() && !found){
+        if (l.getText().equalsIgnoreCase(this.scheduler.lectures.get(i).name)){
+          found = true;
+          lectures.add(this.scheduler.lectures.get(i));
+        }
+        i++;
+      }
+      if (!found){
+        throw new IllegalArgumentException("LECTURE " + l.getText() + " DOES NOT EXIST! CREATE ONE.\n");
+      }
+    }
+    ctx.class_name().size();
+    Lecture[] lectures_arr = lectures.toArray((new Lecture[lectures.size()]));
+    scheduler.addConstraint(new Constraint(lectures_arr));
     super.exitMake_constrain(ctx);
+    System.out.printf("CONSTRAINS ADDED.\n");
   }
 }
