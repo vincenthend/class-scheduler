890b7dd09a6debce9655b42444bde62ba37817f7
vincenthend <vincenthendrha@gmail.com>
2018-11-22 20:42:41 +0700

    Added interactive console

--- src/main/java/scheduler/controller/Scheduler.java
+++ src/main/java/scheduler/controller/Scheduler.java
@@ -1,5 +1,6 @@
 package scheduler.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import scheduler.model.Classroom;
 import scheduler.model.Constraint;
@@ -9,29 +10,34 @@ import scheduler.model.Schedule;
 
 public class Scheduler {
 
-    public List<Lecture> lectures;
-    public List<Classroom> classrooms;
-    public List<Constraint> constraints;
-    public List<Lecturer> lecturers;
-    private Schedule[] schedules;
+  public List<Lecture> lectures;
+  public List<Classroom> classrooms;
+  public List<Constraint> constraints;
+  public List<Lecturer> lecturers;
+  private Schedule[] schedules;
 
-    public Scheduler() {}
+  public Scheduler() {
+    lectures = new ArrayList<>();
+    classrooms  = new ArrayList<>();
+    constraints = new ArrayList<>();
+    lecturers = new ArrayList<>();
+  }
 
-    public void addLecture(Lecture lecture) {
-        this.lectures.add(lecture);
-    }
+  public void addLecture(Lecture lecture) {
+    this.lectures.add(lecture);
+  }
 
-    public void addClassroom(Classroom classroom) {
-        this.classrooms.add(classroom);
-    }
+  public void addClassroom(Classroom classroom) {
+    this.classrooms.add(classroom);
+  }
 
-    public void addConstraint(Constraint constraint) {
-        this.constraints.add(constraint);
-    }
+  public void addConstraint(Constraint constraint) {
+    this.constraints.add(constraint);
+  }
 
-    public void addLecturer(Lecturer lecturer){
-        this.lecturers.add(lecturer);
-    }
+  public void addLecturer(Lecturer lecturer) {
+    this.lecturers.add(lecturer);
+  }
 
     /* Scheduling */
 
