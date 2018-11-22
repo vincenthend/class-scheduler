6da013965750fac8c27333d5a050b8ec04fcda53
Reinhard Linardi <reinhardlinardi@gmail.com>
2018-11-22 20:04:22 +0700

    Pretty print done

--- src/main/java/scheduler/App.java
+++ src/main/java/scheduler/App.java
@@ -1,10 +1,7 @@
 package scheduler;
 
 import scheduler.controller.Scheduler;
-import scheduler.model.Classroom;
-import scheduler.model.Lecture;
-import scheduler.model.Lecturer;
-import scheduler.model.Schedule;
+import scheduler.model.*;
 import scheduler.model.enums.Facility;
 import scheduler.model.type.Time;
 import scheduler.util.ScheduleTable;
@@ -17,21 +14,25 @@ import java.time.DayOfWeek;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
         // Read parser
 
         // Serialize object
 
         // Scheduling
         try {
-            Classroom classroom = new Classroom("7602", 50, Facility.WHITEBOARD, Facility.PROJECTOR);
+            // TODO : Remove these test data
+            Classroom room7602 = new Classroom("7602", 50, Facility.WHITEBOARD, Facility.PROJECTOR);
+            Classroom room7603 = new Classroom("7603", 40, Facility.WHITEBOARD);
             Lecturer dessi = new Lecturer("Dessi", new Time(DayOfWeek.SUNDAY, 7, 13));
+            Lecturer windy = new Lecturer("Windy", new Time(DayOfWeek.SUNDAY, 7, 8));
             Lecture ml = new Lecture("ML", dessi, 50, Facility.PROJECTOR);
+            Lecture sil = new Lecture("SIL", windy, 30, Facility.WHITEBOARD);
+            Constraint constraint = new Constraint(ml, sil);
 
             Scheduler scheduler = new Scheduler();
-            scheduler.setLectures(ml);
-            scheduler.setClassrooms(classroom);
-            scheduler.setConstraints();
+            scheduler.setLectures(ml, sil);
+            scheduler.setClassrooms(room7602, room7603);
+            scheduler.setConstraints(constraint);
 
             ScheduleTable table = new ScheduleTable(scheduler.schedule());
             table.print();
