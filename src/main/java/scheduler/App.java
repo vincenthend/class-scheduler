975924c6a109e2ed5af85b3d405c306777886527
vincenthend <vincenthendrha@gmail.com>
2018-11-22 13:04:43 +0700

    Added parsing for createClassroom

--- src/main/java/scheduler/App.java
+++ src/main/java/scheduler/App.java
@@ -1,22 +1,33 @@
 package scheduler;
 
-import scheduler.controller.Scheduler;
-import scheduler.model.Schedule;
+import java.util.Scanner;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import scheduler.controller.SchedulerActionListener;
+import scheduler.parser.SchedulerLexer;
+import scheduler.parser.SchedulerParser;
 
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        // Read parser
 
-        // Serialize object
+  public static void main(String[] args) {
+    SchedulerActionListener listener = new SchedulerActionListener();
 
-        // Schedule
-        Schedule[] schedules = Scheduler.schedule(null, null);
+    // Read parser
+    Scanner s = new Scanner(System.in);
+    String input = s.nextLine();
+    CharStream cs = CharStreams.fromString(input);
 
-        // Print Schedules
+    // Tokenize and build parse tree
+    SchedulerLexer lexer = new SchedulerLexer(cs);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-    }
+    SchedulerParser parser = new SchedulerParser(tokens);
+    // Serialize object and Schedule
+    parser.addParseListener(listener);
+    parser.start();
+  }
 }
